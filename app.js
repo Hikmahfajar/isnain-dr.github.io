c4e0f8886d413f5fd58f0ea60e21272b5b61db59
isnain-dr <58287881+isnain-dr@users.noreply.github.com>
2020-05-12 23:25:40 +0700

    app
    Update app

---
+++ app.js
@@ -0,0 +1,24 @@
+import "https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js";
+const mapbox_token = "pk.eyJ1IjoibmFpbmRyIiwiYSI6ImNrOWtydzl4dDAyaHUzbW9jeGQ0Z2M3a2YifQ.lJBv65Gcd0aNiD4kS0kYEQ";
+
+
+
+mapboxgl.accessToken = mapbox_token;
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+fetch("/get-covid.json")
+.then(response => response.json())
+.then(data => { 
+    const {places, reports} = data;
+
+reports
+.filter(report => !report.hide)
+.forEach(report => {
+    const {infected, placeId} = report;
+    const curruntPlace = place.find(place => place.id == placeId);
+    console.log(infected, curruntPlace);
+    }); 
+});
